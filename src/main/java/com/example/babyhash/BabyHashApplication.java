a385c19e036d758c770098d9be3306c18d720a99
kali973 <gilles.coutard@outlook.fr>
2023-02-16 20:48:57 +0100

    Initialisation du projet

--- src/main/java/com/example/babyhash/BabyHashApplication.java
+++ src/main/java/com/example/babyhash/BabyHashApplication.java
@@ -3,81 +3,73 @@ package com.example.babyhash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class BabyHashApplication {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         SpringApplication.run(BabyHashApplication.class, args);
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int choice = 0;
 
-        while (true) {
-            System.out.println("BabyHash Menu:");
-            System.out.println("1. Generate hash for input data");
-            System.out.println("2. Quit");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline left-over
+        do {
+            System.out.println("BabyHash Menu");
+            System.out.println("1. Generate hash");
+            System.out.println("2. Calculate block hash");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = Integer.parseInt(reader.readLine());
 
             switch (choice) {
                 case 1:
-                    generateHash(scanner);
+                    System.out.println("Enter some data for a small hash generation");
+                    System.out.println("For BabyHash, all input data is converted to lower case");
+                    Date var1 = new Date();
+                    long var2 = var1.getTime();
+                    new GregorianCalendar(2018, 10, 28);
+                    String var10 = reader.readLine();
+                    var10 = var10.toLowerCase();
+                    String var11 = "FFFF";
+
+                    for(int var12 = 0; var11.compareTo("0000") != 0; ++var12) {
+                        var10 = var10.concat(Integer.toString(var12));
+                        String var13 = ComputeSHA_256_as_Hex_String(var10);
+                        var11 = var13.substring(0, 4);
+                        System.out.println(var13.toUpperCase());
+                        System.out.println("iteration number:" + var12);
+                    }
+
+                    Date var14 = new Date();
+                    long var7 = var14.getTime() - var2;
+                    System.out.println("Mining time = " + var7 / 1000L + " sec");
                     break;
                 case 2:
-                    System.out.println("Goodbye!");
-                    System.exit(0);
-                case 3:
-                    runPythonProgram();
+                    System.out.println("Calling Python program to calculate block hash...");
+                    // Code to call external Python program goes here
                     break;
-
+                case 3:
+                    System.out.println("Exiting BabyHash...");
+                    break;
                 default:
-                    System.out.println("Invalid choice, please try again.");
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
             }
-        }
-    }
 
-    private static void runPythonProgram() {
-        try {
-            ProcessBuilder pb = new ProcessBuilder("python", "/chemin/vers/le/programme.py");
-            pb.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void generateHash(Scanner scanner) {
-        System.out.println("Enter some data for a small hash generation");
-        System.out.println("For BabyHash, all input data is converted to lower case");
-        Date var1 = new Date();
-        long var2 = var1.getTime();
-        new GregorianCalendar(2018, 10, 28);
-        String var10 = scanner.nextLine().toLowerCase();
-        String var11 = "FFFF";
-
-        for(int var12 = 0; !var11.equals("0000"); ++var12) {
-            String var13 = var10.concat(Integer.toString(var12));
-            String var14 = ComputeSHA_256_as_Hex_String(var13);
-            var11 = var14.substring(0, 4);
-            System.out.println(var14.toUpperCase());
-            System.out.println("iteration number:" + var12);
-        }
-
-        Date var15 = new Date();
-        long var7 = var15.getTime() - var2;
-        System.out.println("Mining time = " + var7 / 1000L + " sec");
+        } while (choice != 3);
     }
 
     public static String ComputeSHA_256_as_Hex_String(String var0) {
         try {
-            MessageDigest var1 = MessageDigest.getInstance("SHA-256"); // Correction : utiliser SHA-256 au lieu de SHA-1
+            MessageDigest var1 = MessageDigest.getInstance("SHA-1");
             var1.update(var0.getBytes("UTF-8"), 0, var0.length());
             byte[] var2 = var1.digest();
             return convertToHex(var2);
