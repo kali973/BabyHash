07f170e567d1cdfb2a46cf92a6d77d7bc0cca178
kali973 <gilles.coutard@outlook.fr>
2023-02-19 12:13:47 +0100

    Initialisation du projet

--- src/main/java/com/example/babyhash/BabyHashApplication.java
+++ src/main/java/com/example/babyhash/BabyHashApplication.java
@@ -15,6 +15,11 @@ import java.util.GregorianCalendar;
 
 @SpringBootApplication
 public class BabyHashApplication {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         SpringApplication.run(BabyHashApplication.class, args);
@@ -41,14 +46,54 @@ public class BabyHashApplication {
                     var10 = var10.toLowerCase();
                     String var11 = "FFFF";
 
-                    for (int var12 = 0; var11.compareTo("0000") != 0; ++var12) {
+                    boolean valuesPrintedDeux = false;
+                    boolean valuesPrintedTrois = false;
+                    boolean valuesPrintedQuatre = false;
+                    boolean valuesPrintedCinq = false;
+
+                    System.out.println("");
+
+                    for (int var12 = 0; var11.compareTo("00000") != 0; ++var12) {
                         var10 = var10.concat(Integer.toString(var12));
                         String var13 = ComputeSHA_256_as_Hex_String(var10);
                         var11 = var13.substring(0, 4);
-                        System.out.println(var13.toUpperCase());
-                        System.out.println("iteration number:" + var12);
-                    }
+                        String var22 = var13.substring(0, 5);
+                        System.out.print(var13.toUpperCase() + " - iteration number:" + var12 + "\r");
 
+                        if (var22.substring(0, 5).compareTo("00000") == 0 & !valuesPrintedCinq) {
+                            valuesPrintedCinq = true;
+                            Date var14 = new Date();
+                            long var7 = var14.getTime() - var2;
+                            System.out.println(ANSI_RED + " ==========> " + var13.toUpperCase() + ANSI_RESET);
+                            System.out.println(ANSI_RED +" ==========> Mining time = " + var7 / 1000L + " sec" + ANSI_RESET);
+                            System.out.println(ANSI_RED +" ==========> Number of iterations: " + var12 + ANSI_RESET);
+                            System.out.println("");
+                        } else if (var22.substring(0, 4).compareTo("0000") == 0 & !valuesPrintedQuatre) {
+                            valuesPrintedQuatre = true;
+                            Date var14 = new Date();
+                            long var7 = var14.getTime() - var2;
+                            System.out.println(ANSI_BLUE + " ==========> " + var13.toUpperCase() + ANSI_RESET);
+                            System.out.println(ANSI_BLUE +" ==========> Mining time = " + var7 / 1000L + " sec" + ANSI_RESET);
+                            System.out.println(ANSI_BLUE +" ==========> Number of iterations: " + var12 + ANSI_RESET);
+                            System.out.println("");
+                        }else if (var22.substring(0, 3).compareTo("000") == 0 & !valuesPrintedTrois) {
+                            valuesPrintedTrois = true;
+                            Date var14 = new Date();
+                            long var7 = var14.getTime() - var2;
+                            System.out.println(ANSI_PURPLE + " ==========> " + var13.toUpperCase() + ANSI_RESET);
+                            System.out.println(ANSI_PURPLE +" ==========> Mining time = " + var7 / 1000L + " sec" + ANSI_RESET);
+                            System.out.println(ANSI_PURPLE +" ==========> Number of iterations: " + var12 + ANSI_RESET);
+                            System.out.println("");
+                        }else if (var22.substring(0, 2).compareTo("00") == 0 & !valuesPrintedDeux) {
+                            valuesPrintedDeux = true;
+                            Date var14 = new Date();
+                            long var7 = var14.getTime() - var2;
+                            System.out.println(ANSI_GREEN + " ==========> " + var13.toUpperCase() + ANSI_RESET);
+                            System.out.println(ANSI_GREEN +" ==========> Mining time = " + var7 / 1000L + " sec" + ANSI_RESET);
+                            System.out.println(ANSI_GREEN +" ==========> Number of iterations: " + var12 + ANSI_RESET);
+                            System.out.println("");
+                        }
+                    }
                     Date var14 = new Date();
                     long var7 = var14.getTime() - var2;
                     System.out.println("Mining time = " + var7 / 1000L + " sec");
